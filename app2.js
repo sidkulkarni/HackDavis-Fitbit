bf343526458c2b95e45f4f052390289d7292e800
Siddharth Kulkarni <sidkulkarni@ucdavis.edu>
2019-02-10 03:52:12 -0800

    Add files via upload

--- app2.js
+++ app2.js
@@ -41,10 +41,10 @@ xhr.send(params);
 }
 
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-//const url = "https://example.com"; // site that doesn’t send Access-Control-*
-fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-.then(response => response.text())
-.then(contents => console.log(contents))
-.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-// Run code snippet
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// //const url = "https://example.com"; // site that doesn’t send Access-Control-*
+// fetch(proxyurl) // https://cors-anywhere.herokuapp.com/https://example.com
+// .then(response => response.text())
+// .then(contents => console.log(contents))
+// .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+// // Run code snippet
